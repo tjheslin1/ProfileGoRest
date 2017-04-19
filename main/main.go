bc9263b03a884ecf387710ee930e16965b98e176
Thomas Heslin <tjheslin1@gmail.com>
2017-04-19 16:02:51 +0100

    Basic rest server, can be hit at /hello and request is logged

--- main/main.go
+++ main/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, go!")
+	http.HandleFunc("/hello", helloHandler)
+
+	go startServer()
+
+	var input string
+	fmt.Scanln(&input)
+}
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	log.Println(req)
+	io.WriteString(w, "hello, world!\n")
+}
+
+func startServer() {
+	err := http.ListenAndServe(":6060", nil)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
 }
